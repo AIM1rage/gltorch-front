0a333a115f49f78aa2b89e134d7dee624aa43da3
Mikhail Kozlov <wmikkoz@icloud.com>
2024-12-12 13:16:30 +0500

    fix? notices

--- src/store/notice.ts
+++
@@ -1,7 +0,0 @@
-import { NoticeProps } from "@/components/ui/notice";
-
-type NoticeStore = {
-  notices: NoticeProps[];
-  addNotice: (notice: NoticeProps) => void;
-  removeNotice: (notice: NoticeProps) => void;
-};
